03e82fb7904ebd37e47dd443e20e0dc701c4fc5e
hongdonghyun <hong4367@gmail.com>
2020-03-20 12:09:57 +0900

    경로 수정 및 세팅 수정

---
+++ MarketBroccoli/MarketBroccoli/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MarketBroccoli
+//
+//  Created by Hongdonghyun on 2020/03/20.
+//  Copyright © 2020 Team3. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
