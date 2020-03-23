49238fd6abd39f84374fb3615e5cc887d516a250
hongdonghyun <hong4367@gmail.com>
2020-03-23 14:14:51 +0900

    HomeView 첫번째셀 (HomeImageTableCell)

---
+++ MarketBroccoli/MarketBroccoli/Source/Cells/HomeViewCells/HomeImageTableCell.swift
@@ -0,0 +1,20 @@
+//
+//  HomeImageTableCell.swift
+//  MarketBroccoli
+//
+//  Created by Hongdonghyun on 2020/03/23.
+//  Copyright © 2020 Team3. All rights reserved.
+//
+
+import UIKit
+
+class HomeImageTableCell: UITableViewCell {
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
