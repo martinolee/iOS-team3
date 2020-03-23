c10e081d4b9b65b7406888d15af9179fbf85055a
anjh23491 <anjh23491@gmail.com>
2020-03-23 12:30:17 +0900

    hard work

--- MarketBroccoli/MarketBroccoli/Source/ViewControllers/SignUpViewController.swift
+++ MarketBroccoli/MarketBroccoli/Source/ViewControllers/SignUpViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    private let signupTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+      setupUI()
+    }
+  private func setupUI() {
+    [signupTableView].forEach {
+      view.addSubview($0)
     }
     
+    signupTableView.rowHeight = 120
+    
+    signupTableView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+     
+    signupTableView.dataSource = self
+    signupTableView.register(SignupTableViewCell.self, forCellReuseIdentifier: SignupTableViewCell.identifier)
+  }
+}
 
+extension SignUpViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard indexPath.row == 0 else { return UITableViewCell() }
+    let cell = tableView.dequeueReusableCell(withIdentifier: SignupTableViewCell.identifier, for: indexPath) as! SignupTableViewCell
+    return cell
+  }
 }
