a0b5f35ba75cdc8acf85940adf786fe306d4310b
hongdonghyun <hong4367@gmail.com>
2020-04-07 16:51:32 +0900

    디테일 뷰 코드 구조 변경

---
+++ MarketBroccoli/MarketBroccoli/Source/Details/DetailCells/DetailDescriptionBottomTableCell.swift
@@ -0,0 +1,12 @@
+//
+//  DetailDescriptionBottomTableCell.swift
+//  MarketBroccoli
+//
+//  Created by Hongdonghyun on 2020/04/07.
+//  Copyright © 2020 Team3. All rights reserved.
+//
+
+import UIKit
+
+class DetailDescriptionBottomTableCell: UITableViewCell {
+}
