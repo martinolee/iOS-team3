c14ccb079ef5581637d84fb710982b607b7746ec
hongdonghyun <hong4367@gmail.com>
2020-04-08 21:38:48 +0900

    테스트용 플레이그라운드 추가

---
+++ MarketBroccoli/MarketBroccoli/Source/CustomUI/Details/WhyKurlyView.swift
@@ -0,0 +1,21 @@
+//
+//  WhyKurlyView.swift
+//  MarketBroccoli
+//
+//  Created by Hongdonghyun on 2020/04/08.
+//  Copyright © 2020 Team3. All rights reserved.
+//
+
+import UIKit
+
+class WhyKurlyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
