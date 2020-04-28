c2f5538bae6f355f61bfb438bb082a7013693ffd
Hailey <foxwavez@gmail.com>
2020-04-28 22:33:47 +0900

    로직 바꿔서 불필요한 코드 삭제

--- MarketBroccoli/MarketBroccoli/Source/ViewControllers/CategoryDetailViewController.swift
+++ MarketBroccoli/MarketBroccoli/Source/ViewControllers/CategoryDetailViewController.swift
@@ -221,36 +221,11 @@ extension CategoryDetailViewController: UICollectionViewDataSource {
       cell.subConfigure(subID: subCategoryID)
       return cell
     }
-//    switch collectionViewSubCategoryId {
-//    case 0:
-//      let cell = collectionView.dequeue(CategoryDetailCollectionViewCell.self, indexPath: indexPath)
-//      print("0번째 didSet categoryID", categoryId)
-//      print("컬렉션뷰서브카테고리", collectionViewSubCategoryId)
-//      print("전체보기", indexPath.row)
-//      cell.configure(id: categoryId)
-//      return cell
-//    default:
-//      switch indexPath.row {
-//      case 0:
-//        let cell = collectionView.dequeue(CategoryDetailCollectionViewCell.self, indexPath: indexPath)
-//        cell.configure(id: categoryId)
-//        print("몇번 째를 눌렀나 indexPath.row[0]", collectionViewSubCategoryId - 1)
-//        return cell
-//      default:
-//        let cell = collectionView.dequeue(CategoryDetailCollectionViewCell.self, indexPath: indexPath)
-//        let subCategoryIncerease = categoryData[0..<categoryId - 1].reduce(0) { $0 + $1.row.count } - categoryId + 1
-//        print("서브 카테고리의 카운트", categoryData[0..<categoryId - 1].reduce(0) { $0 + $1.row.count })
-//        print("0번째 아님didSet subCategoryIncerease", subCategoryIncerease)
-//        let subCategoryID = subCategoryIncerease + indexPath.row
-//        print("indexPath.row[1~]", collectionViewSubCategoryId - 1)
-//        print("0번째 아님didSet subCategoryID", subCategoryID)
-//        cell.subConfigure(subID: subCategoryID)
-//        return cell
-//      }
-//    }
+
   }
 }
 
-// MARK: - ACTIONS
-//  extension CategoryDetailViewController {
-//}
+ // MARK:- ACTIONS
+  extension CategoryDetailViewController {
+    
+}
