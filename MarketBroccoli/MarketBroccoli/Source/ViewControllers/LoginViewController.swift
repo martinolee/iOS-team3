6da56c80edeacba37b7fd182504c06009dcb21df
anjh23491 <anjh23491@gmail.com>
2020-04-28 17:34:49 +0900

    프로필 이름 수정

--- MarketBroccoli/MarketBroccoli/Source/ViewControllers/LoginViewController.swift
+++ MarketBroccoli/MarketBroccoli/Source/ViewControllers/LoginViewController.swift
@@ -74,6 +74,7 @@ extension LoginViewController: LoginViewDelegate {
                 ) else { return }
               
               UserDefaultManager.shared.set(decodedData.token, for: .token)
+              UserDefaultManager.shared.set(decodedData.user.name, for: .userName)
               print(decodedData)
               
               guard
